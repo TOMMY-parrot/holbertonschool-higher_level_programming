61b0d8547b16ef45ea9f262d8451393b07310fdd
root <root@yassin>
2023-09-25 17:35:31 +0100

    e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-for letter in range(97, 123):
-    if letter != 101 and letter != 113:
-        print(chr(letter), end = "")
+#!/usr/bin/python3
+for i in range(97, 123):
+    if i != 113 and i != 101:
+        print("{}".format(chr(i)), end='')
