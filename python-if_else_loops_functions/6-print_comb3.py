d0d10a6dd416b4e9ba9a0036aebaabaf59be20e6
root <root@yassin>
2023-09-26 15:11:21 +0100

    e

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for num in range(10):
-    for num2 in range(num, 10):
-        if num < num2:
-            print("{:d}{:d}".format(num, num2),
-                  end="\n" if num is 8 and num2 is 9 else ", ")
+for i in range(0, 9):
+    for n in range(i + 1, 10):
+        if i < 8:
+            print("{}{}, ".format(i, n), end="")
+print("89")
