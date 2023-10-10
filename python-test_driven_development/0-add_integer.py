50c5dd24669bf4ad650f9275d720e28a17a0bcd6
root <root@yassin>
2023-10-10 13:27:35 +0100

    class_rectangle

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Integers addition
+function that adds 2 integers
+add two integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add two integer
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
