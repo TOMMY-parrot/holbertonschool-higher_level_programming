788022cf7778a66d74484a1ae37d76d09365fe4b
root <root@yassin>
2023-10-19 11:57:37 +0100

    e

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module for class MyList"""
+"""
+Write a class MyList that inherits from list:
+    *Public instance method: def print_sorted(self): that prints the list,
+    but sorted (ascending sort)
+    *You can assume that all the elements of the list will be of type int
+    *You are not allowed to import any module
+"""
 
 
 class MyList(list):
-    """class MyList that inherits from list"""
-
+    """This function print every method of the super class"""
     def print_sorted(self):
-        """Method for printing sorted list"""
         print(sorted(self))
