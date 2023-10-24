7dd90c955228a6036a22029d6e9e24911ee38395
root <root@yassin>
2023-10-24 12:59:52 +0100

    almost a cercle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -44,7 +44,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
     
     def update(self, *args, **kwargs):
         """
