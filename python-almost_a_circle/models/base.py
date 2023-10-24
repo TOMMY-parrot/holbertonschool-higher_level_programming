2bcdf03c1e236d501786bdc4c88984229b8aa480
root <root@yassin>
2023-10-24 11:44:25 +0100

    almost a cercle

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Create a folder named models with an empty file __init__.py
+inside - with this file, the folder will become a Python module
+'''
+
+import json
+import csv
+
+
+class Base:
+    '''
+    Base class
+    '''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''
+        Constructor
+        '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
