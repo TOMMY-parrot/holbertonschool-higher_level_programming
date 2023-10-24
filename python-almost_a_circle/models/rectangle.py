6a944d00c82bea51c83e94765c86ae07c0cf1253
root <root@yassin>
2023-10-24 11:59:24 +0100

    almost a cercle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Defines a rectangle class."""
+'''
+Write the class Rectangle that
+inherits from Base:
+'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Represent a rectangle."""
+    '''
+    Class Rectangle inherits from Base
+    '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
-        """
+        '''
+        Constructor
+        '''
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
