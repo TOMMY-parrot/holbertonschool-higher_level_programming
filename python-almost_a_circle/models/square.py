04365135fcac1e55c082f5eaa675f222068be12b
root <root@yassin>
2023-10-24 12:37:40 +0100

    almost a cercle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module that defines a square """
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """ Class that represents a square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square's position.
+            y (int, optional): The y-coordinate of the square's position.
+            id (int, optional): The unique identifier for the square.
+
+        Returns:
+            None
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square.
+
+        Returns:
+            str: A string in the format "[Square] (<id>) <x>/<y> - <size>"
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+if __name__ == "__main__":
+    s = Square(5, 2, 3, 1)
+    print(s)
