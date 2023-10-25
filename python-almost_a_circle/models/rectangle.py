b11378e5c32bb436b1e1122f11180bfed6a1e2b9
root <root@yassin>
2023-10-25 12:34:52 +0100

    almost a cercle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# rectangle.py
 """Defines a rectangle class."""
 from models.base import Base
 
@@ -84,43 +85,62 @@ class Rectangle(Base):
         return self.width * self.height
 
     def display(self):
-        """
-        Prints the rectangle with # characters to stdout.
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
 
-        Returns:
-            None
-        """
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def __str__(self):
-        '''
-        String representation
-        '''
-        return ('[Rectangle] ({}) {}/{} - {}/{}'.format(
-            self.id, self.x, self.y, self.width, self.height))
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
 
     def update(self, *args, **kwargs):
-        """
-        Updates the attributes of the Rectangle.
+        """Update the Rectangle.
 
         Args:
-            *args: No-keyword arguments representing the attribute values
-            **kwargs: Key-worded arguments representing the attribute values
-
-        Returns:
-            None
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
         """
-        if args:
-            attrs = ["id", "width", "height", "x", "y"]
-            for i, attr_value in enumerate(args):
-                setattr(self, attrs[i], attr_value)
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
         """Return the dictionary representation of a Rectangle."""
@@ -130,4 +150,10 @@ class Rectangle(Base):
             "height": self.height,
             "x": self.x,
             "y": self.y
-        }                
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
