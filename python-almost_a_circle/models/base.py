d29cf4a7540d1d1bb08359511bac7e402e9e9847
root <root@yassin>
2023-10-27 11:32:54 +0100

    creation square or a rectangle dummy

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,12 +26,11 @@ class Base:
             self.id = Base.__nb_objects
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts.
-
-        Args:
-            list_dictionaries (list): A list of dictionaries.
-        """
-        if list_dictionaries is None or list_dictionaries == []:
+        '''
+        Turn dictionary representation
+        into JSON
+        '''
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
     
